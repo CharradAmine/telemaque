6f2865802b4342a5da6e09d946524d7ba084f6a5
dev <dev@192.168.4.38>
2015-06-23 15:28:47 +0200

    création de deux bundles : FrontBundle et AdminBundle

---
+++ src/Ajy/AdminBundle/AjyAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ajy\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AjyAdminBundle extends Bundle
+{
+}
---
+++ src/Ajy/FrontBundle/AjyFrontBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ajy\FrontBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AjyFrontBundle extends Bundle
+{
+}
